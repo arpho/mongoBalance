de4230d1e445d8cb1e288f0ab6c441f6b4fd4549
giuseppe <damicogiuseppe77@gmail.com>
2013-12-20 09:31:58 +0100

    0.1.7 deep refactoring the controllers has been splitted in two files

---
+++ app/scripts/controllers/add.js
@@ -0,0 +1,30 @@
+'use strict';
+var debug = function (s) {
+    console.log(s);
+}
+angular.module('mongoBalanceApp').controller('AddCtrl', function ($scope, $http) {
+    $scope.item = 'item';
+    $scope.pushCategory = function(v) { 
+        debug('categoria selezionata '+v);
+        if (typeof($scope.selected_category)=='undefined') {
+            $scope.selected_category=[v];
+        }
+        else {
+                $scope.selected_category.push(v);
+                debug($scope.selected_category);
+             }
+    }
+    $scope.submitForm = function() {
+        debug('on submit');
+        debug($scope.selected_category);
+        var item = {};
+        item.item = $scope.item;
+        item.info = $scope.info;
+        item.price = $scope.price;
+        item.payment = $scope.newPayment || $scope.Payment;
+        item.category = $scope.selected_category;
+       // item.category.push($scope.newCategory|| $scope.Category);
+        $http.post('/api/addPurchase',item).success(function(u) {debug('purchase sent to server');})
+        debug(item);
+    }
+});
